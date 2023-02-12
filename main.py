52cfe7f848a48beafaa5834066233f769af49fa2
Alireza <armanparsi206@outlook.com>
2023-02-12 21:33:37 +0330

    The version that works

--- main.py
+++ main.py
@@ -1,50 +1,70 @@
 from PIL import Image
 from tkinter import filedialog
-import PIL
-import os
-import glob
-import time
+import tkinter.filedialog as fd
 import concurrent.futures
 
 
 class ConvertImage():
-    def __init__(self , PathInputFolder , PathOutputFolder , ForamtOutput , FAST=True) -> None:
+    def __init__(self , ImagesPath, PathOutputFolder , ForamtOutput , FAST=True , Images=dict()) -> None:
         self.FAST = FAST
-        self.PathInput = PathInputFolder
+        # self.PathInput = PathInputFolder # (This feature will be added in future updates)
         self.PathOutput = PathOutputFolder
         self.FormatsInput  = ('jpg' , 'png' , 'webp')
         self.FormatsOutput = ('jpg' , 'png' , 'webp')
         self.FormatOutout = ForamtOutput
+        self.Images = Images
+        self.ImagesPath = ImagesPath
     
-    def FindImages(self):
-        # Get the files and folders in the input folder
-        ListDir = os.listdir(self.PathInput)
+    # Extract images from the folder (This feature will be added in future updates)
+    # def FindImages(self):
+    #     # Get the files and folders in the input folder
+    #     ListDir = os.listdir(self.PathInput)
         
-        Images = list()
-        # Filter photos
-        for item in ListDir :
+    #     # Filter photos
+    #     for item in ListDir :
+    #         item = item.split('.')
+    #         if item[-1] in self.FormatsInput:
+    #             name = ''
+    #             for l in item[:-1]: name += '.' + l
+    #             name = name[1:]
+                
+    #             self.Images[str(name)] = {
+    #                 'name' : name ,
+    #                 'path' : f'{self.PathInput}/{name}.{item[-1]}',
+    #                 'format' : item[-1]
+    #                 }
+
+    def OrganizeImagesAddress(self):
+        for item in self.ImagesPath :
+            
             item = item.split('.')
             if item[-1] in self.FormatsInput:
                 name = ''
                 for l in item[:-1]: name += '.' + l
                 name = name[1:]
-                Images.append((name,item[-1]))
-        
-        self.Images = Images
+                
+                self.Images[str(name)] = {
+                    'name' : name.split('/')[-1] ,
+                    'path' : f'{name}.{item[-1]}',
+                    'format' : item[-1]
+                    }
+
     def ConvertImage(self , Photo):
         try :
+            print(Photo['path'])
             # Open The Image
-            Img = Image.open(f'{self.PathInput}/{Photo[0]}.{Photo[1]}')
+            Img = Image.open(Photo['path'])
             # Convert the image to RGB Colour
             Img = Img.convert('RGB')
             # Save The Image
-            Img.save(f'{self.PathOutput}/{Photo[0]}.{self.FormatOutout}' , self.FormatOutout)
+            Img.save(f'{self.PathOutput}/{Photo["name"]}.{self.FormatOutout}' , self.FormatOutout)
+            print('Done')
         except OSError : return False
     
     def Run(self):
         if self.FAST == True:
             with concurrent.futures.ThreadPoolExecutor() as executor:
-                Tasks = [executor.submit(self.ConvertImage , img) for img in self.Images]
+                Tasks = [executor.submit(self.ConvertImage , img) for img in self.Images.values()]
 
                 for task in Tasks:
                     task.result()
@@ -56,11 +76,22 @@ class ConvertImage():
             return
 
 
-PathInput = filedialog.askdirectory(title="Select Drictory For Save")
-PathOutput = filedialog.askdirectory(title="Select Drictory For Save")
-test = ConvertImage(PathInput , PathOutput , 'webp')
-Start = time.time()
-test.FindImages()
-print(test.Images[1])
+
+InPath = fd.askopenfilenames(title='Choose a file' ,  filetypes=[
+                    ("All Images", ".jpg"),("All Images", ".png"),("All Images", ".webp"),
+                    ("JPG", ".jpg"),("PNG", ".png"),("WEBP", ".webp"),])
+
+print(InPath)
+OutPath = filedialog.askdirectory(title="Select Drictory For Save")
+test = ConvertImage(ImagesPath=InPath , PathOutputFolder=OutPath , ForamtOutput='webp')
+test.OrganizeImagesAddress()
+print(test.Images)
+print(OutPath)
 test.Run()
-print(time.time() - Start)
+
+
+
+
+
+
+

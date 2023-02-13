ee5a2075c8f1cbd0f1c912aead4b380d438f0b9a
Alireza <armanparsi206@outlook.com>
2023-02-13 13:57:47 +0330

    Completion Of The Image & Folder Selection Section

--- GUI.py
+++ GUI.py
@@ -1,9 +1,12 @@
 from tkinter import * 
-import tkinter.font as font
 from tkinter import ttk
-import tkinter.filedialog as fd
+from tkinter import filedialog
 from threading import Thread
+
+import tkinter.font as font
+import tkinter.filedialog as fd
 import main
+import os
 
 class GUI():
     def __init__(self) -> None:
@@ -22,7 +25,23 @@ class GUI():
         for button in ButtonsFormat:
             if button[0] == Button : Button['bg'] = '#FFFF00'
             else : button[0]['bg'] = button[1]
-
+                
+    def SelectImages(self):
+        images = fd.askopenfilenames(title='Choose a file' ,  filetypes=[
+                        ("All Images", ".jpg"),("All Images", ".png"),("All Images", ".webp"),
+                        ("JPG", ".jpg"),("PNG", ".png"),("WEBP", ".webp"),])
+        self.Images = images
+        # Size Calculation
+        Total , Size = 0 , 0
+        for img in images : Total , Size = Total + 1 , Size + os.path.getsize(img)
+        Size = (Size / 1024) / 1024
+        self.Label_TotalImagesAndSize = Label(self.FreameINPandOUT , text=f'Total : {Total} {" "* ( 10 - len(str(Total)))} Size : {round(Size , 2)} MB' , bg='#48E6E1' , font=font.Font(family='Comic Sans MS', size=10 , weight='bold'))
+        self.Label_TotalImagesAndSize.place(x=30 , y=50)
+    
+    def ChooseDirectorySave(self):
+        self.PathOutputFolder = filedialog.askdirectory(title="Select Drictory For Save")
+        self.Label_ShowPathOutputFolder = Label(self.FreameINPandOUT , text=self.PathOutputFolder , bg='#48E6E1' , font=font.Font(family='Comic Sans MS', size=10 , weight='bold'))
+        self.Label_ShowPathOutputFolder.place(x=30 , y=130)
     def LoadRootWindows(self):
         self.root = Tk()
         self.root.geometry('400x500')
@@ -54,21 +73,21 @@ class GUI():
         self.Button_WEBP.place(x=300 , y=80)
         # Select Images
         FontButtonINPandOUT = font.Font(family='Comic Sans MS', size=10)
-        self.Button_SelectImages = Button(self.FreameINPandOUT , text='Selection' , bg='#27E48C'  , width=8 , height=1 , font=FontButtonINPandOUT)
+        self.Button_SelectImages = Button(self.FreameINPandOUT , text='Selection' , bg='#27E48C'  , width=8 , height=1 , font=FontButtonINPandOUT , command=lambda : Thread(target=self.SelectImages).start())
         self.Button_SelectImages.place(x=305 , y=20)
         # Seve Path
-        self.Button_Seve = Button(self.FreameINPandOUT , text='Seve' , bg='#27E48C'  , width=8 , height=1 , font=FontButtonINPandOUT)
+        self.Button_Seve = Button(self.FreameINPandOUT , text='Seve' , bg='#27E48C'  , width=8 , height=1 , font=FontButtonINPandOUT , command=lambda : Thread(target=self.ChooseDirectorySave).start() )
         self.Button_Seve.place(x=305 , y=100)
         # Start Button
-        self.Button_Seve = Button(self.FreameSTART , text='Start' , bg='#FFFFFF'  , width=6 , height=2 , font=font.Font(family='Comic Sans MS', size=12, weight='bold'))
-        self.Button_Seve.place(x=310 , y=50)
+        self.Button_Start = Button(self.FreameSTART , text='Start' , bg='#FFFFFF'  , width=6 , height=2 , font=font.Font(family='Comic Sans MS', size=12, weight='bold'))
+        self.Button_Start.place(x=310 , y=50)
 
         # ### === >  Labels (Text) < ==== ### #
         Label(self.FreameButtomFormatOutPut , text='Which Format Should Your Images Be Converted To ? ' , bg='#48E6E1' , font=font.Font(family='Comic Sans MS', size=10 , weight='bold')).place(x=30 , y=20)
         
         self.Label_SelectImage = Label(self.FreameINPandOUT , text='Choose The Images You Want >> ' , bg='#48E6E1' , font=font.Font(family='Comic Sans MS', size=10 , weight='bold'))
         self.Label_SelectImage.place(x=30 , y=20)
-        
+
         self.Label_PathSeve = Label(self.FreameINPandOUT , text='Where To Save The Results ?  >> ' , bg='#48E6E1' , font=font.Font(family='Comic Sans MS', size=10 , weight='bold'))
         self.Label_PathSeve.place(x=30 , y=100)
         
